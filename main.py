807319bbf6bd1378e8cb6081bc42e288ca52fbbb
useragents <103281345+useragents@users.noreply.github.com>
2022-04-08 20:26:23 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,93 @@
+import requests, ctypes, time, os, threading
+from colorama import Fore
+
+ascii_text = """
+        _   _ _    _        _    
+       | | (_) |  | |      | |   
+       | |_ _| | _| |_ ___ | | __
+       | __| | |/ / __/ _ \| |/ /
+       | |_| |   <| || (_) |   < 
+        \__|_|_|\_\\__\___/|_|\ _\\
+  """
+
+class tiktok:
+
+    def __init__(self):
+        self.lock = threading.Lock()
+        self.checking = True
+        self.usernames = []
+        self.unavailable = 0
+        self.available = 0
+        self.counter = 0
+
+    def update_title(self):
+        remaining = len(self.usernames) - (self.available + self.unavailable)
+        ctypes.windll.kernel32.SetConsoleTitleW(
+            f"TikTok Username Checker | Available: {self.available} | Unavailable: {self.unavailable} | Checked: {(self.available + self.unavailable)} | Remaining: {remaining} | Developed by @useragents on Github"
+        )
+    
+    def safe_print(self, arg):
+        self.lock.acquire()
+        print(arg)
+        self.lock.release()
+    
+    def print_console(self, status, arg, color = Fore.RED):
+        self.safe_print(f"       {Fore.WHITE}[{color}{status}{Fore.WHITE}] {arg}")
+    
+    def check_username(self, username):
+        with requests.Session() as session:
+            headers = {
+                "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36",
+                "accept-encoding": "gzip, deflate, br",
+                "accept-language": "en-US",
+                "content-type": "application/json"
+            }
+            r = session.head("https://www.tiktok.com/@{}".format(username), headers = headers)
+            if r.status_code == 200:
+                self.unavailable += 1
+                self.print_console("Unavailable", username)
+            elif r.status_code == 404:
+                self.available += 1
+                self.print_console("Available", username, Fore.GREEN)
+            self.update_title()
+        
+    def load_usernames(self):
+        if os.path.exists("usernames.txt"):
+            with open("usernames.txt", "r", encoding = "UTF-8") as f:
+                for line in f.readlines():
+                    line = line.replace("\n", "")
+                    self.usernames.append(line)
+            if not len(self.usernames):
+                self.print_console("Console", "No usernames loaded in usernames.txt")
+                time.sleep(20)
+                os._exit(0)
+        else:
+            self.print_console("Console", "File usernames.txt not found")
+            time.sleep(20)
+            os._exit(0)
+
+    
+    def main(self):
+        os.system("cls")
+        ctypes.windll.kernel32.SetConsoleTitleW("TikTok Username Checker | Developed by @useragents on Github")
+        print(Fore.RED + ascii_text)
+        self.load_usernames()
+        threads = int(input(f"       {Fore.WHITE}[{Fore.RED}Console{Fore.WHITE}] Threads: "))
+        print()
+        if threads >= 20: #To prevent ratelimits
+            threads = 20
+        
+        def thread_starter():
+            self.check_username(self.usernames[self.counter])
+        while self.checking:
+            if threading.active_count() <= threads:
+                try:
+                    threading.Thread(target = thread_starter).start()
+                    self.counter += 1
+                except:
+                    pass
+                if len(self.usernames) <= self.counter:
+                    self.checking = None
+
+obj = tiktok()
+obj.main()
